8d15b55e90aaf96f9e82056c80ac34f2792a3182
Kevin Calderón <josue_29c@hotmail.com>
2022-08-22 19:23:59 -0600

    [FIX] endpoint update y delete.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
-import conn from "./conexion.js"
-import Express from 'express'
-import bodyParser from 'body-parser';
-import cors from "cors"
+import conn from "./conexion.js";
+import Express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = Express();
 var corsOptions = { origin: true, optionsSuccessStatus: 200 };
@@ -9,93 +9,84 @@ app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get("/obtenerProducto", function (req, res) {
+  conn.query("SELECT * FROM producto", function (err, results, fields) {
+    if (err) throw err;
+    else console.log("Selected " + results.length + " row(s).");
 
-
-  app.get('/obtenerProducto',function(req,res){
-    conn.query('SELECT * FROM producto', 
-        function (err, results, fields) {
-            if (err) throw err;
-            else console.log('Selected ' + results.length + ' row(s).');
-            
-            res.send(results)
-            console.log('Done.');
-        })
+    res.send(results);
+    console.log("Done.");
+  });
 });
-app.post('/crearProducto',function(req,res){
-    let nombre =req.body.nombre
-    let descripcion=req.body.descripcion
-    let urlImage=req.body.urlImage
-    let precio=req.body.precio
-    let cantidad =req.body.cantidad
+app.post("/crearProducto", function (req, res) {
+  let nombre = req.body.nombre;
+  let descripcion = req.body.descripcion;
+  let urlImage = req.body.urlImage;
+  let precio = req.body.precio;
+  let cantidad = req.body.cantidad;
 
+  conn.query(
+    "INSERT INTO producto (nombre,descripcion,URLimage,precio,cantidad) VALUES (?,?,?,?,?);",
+    [nombre, descripcion, urlImage, precio, cantidad],
+    function (err, results, fields) {
+      if (err) {
+        console.log(err);
+        return res.send({ reservado: false });
+      } else {
+        console.log("Inserted " + results.affectedRows + " row(s).");
+        return res.send({ reservado: true });
+      }
+    }
+  );
+});
 
-        conn.query('INSERT INTO producto (nombre,descripcion,URLimage,precio,cantidad) VALUES (?,?,?,?,?);', [nombre,descripcion,urlImage,precio,cantidad],
-            function (err, results, fields) {
+app.put("/ActualizarProducto/(:id)", function (req, res) {
+  let id = req.params.id;
+  let nombre = req.body.nombre;
+  let descripcion = req.body.descripcion;
+  let urlImage = req.body.urlImage;
+  let precio = req.body.precio;
+  let cantidad = req.body.cantidad;
 
-        
-        if (err){
-            console.log(err)
-            return res.send({ "reservado": false })
-        }
-        else{
-            console.log('Inserted ' + results.affectedRows + ' row(s).');
-             return res.send({ "reservado": true })
-        }
-        })
+  conn.query(
+    "UPDATE producto SET nombre = ?,descripcion = ?,URLimage = ?,precio = ?,cantidad = ? WHERE idProducto = ?",
+    [nombre, descripcion, urlImage, precio, cantidad, id],
+    function (err, results, fields) {
+      if (err) {
+        console.log(err);
+        return res.send({ actualizado: false });
+      } else {
+        console.log("Updated " + results.affectedRows + " row(s).");
+        return res.send({ actualizado: true });
+      }
+    }
+  );
+});
+app.delete("/EliminarProducto/(:id)", function (req, res) {
+  let id = req.params.id;
 
-})
-
-app.post('/ActualizarProducto',function(req,res){
-    let id =req.body.idProducto
-    let nombre =req.body.nombre
-    let descripcion=req.body.descripcion
-    let urlImage=req.body.urlImage
-    let precio=req.body.precio
-    let cantidad =req.body.cantidad
-
-    conn.query('UPDATE producto SET nombre = ?,descripcion = ?,URLimage = ?,precio = ?,cantidad = ? WHERE idProducto = ?', [nombre,descripcion,urlImage,precio,cantidad,id], 
-            function (err, results, fields) {
-                if (err){
-                    console.log(err)
-                    return res.send({ "actualizado": false })
-                }
-                else{
-                     console.log('Updated ' + results.affectedRows + ' row(s).');
-                     return res.send({ "actualizado": true })
-                }
-           })
-
-})
-app.post('/EliminarProducto',function(req,res){
-    let id =req.body.idProducto
-    
-
-    conn.query('DELETE FROM producto WHERE idProducto = ?', [id], 
-            function (err, results, fields) {
-                if (err){
-                    console.log(err)
-                    return res.send({ "eliminado": false })
-                }
-                else{
-                     console.log('Deleted ' + results.affectedRows + ' row(s).');
-                     return res.send({ "Deleted": true })
-                }
-           })
-
-})
+  conn.query(
+    "DELETE FROM producto WHERE idProducto = ?",
+    [id],
+    function (err, results, fields) {
+      if (err) {
+        console.log(err);
+        return res.send({ eliminado: false });
+      } else {
+        console.log("Deleted " + results.affectedRows + " row(s).");
+        return res.send({ Deleted: true });
+      }
+    }
+  );
+});
 
 app.listen(3000, () => {
-    console.log("El servidor está inicializado en el puerto 3000");
-    conn.connect(
-      function (err) { 
-      if (err) { 
-          console.log("!!! Cannot connect !!! Error:",err);
-          
-      }
-      else
-      {
+  console.log("El servidor está inicializado en el puerto 3000");
+  conn.connect(function (err) {
+    if (err) {
+      console.log("!!! Cannot connect !!! Error:", err);
+    } else {
       console.log("Connection established.");
-          
-      }
+    }
   });
-  });
+});
--- backend/conexion.js
+++ backend/conexion.js
@@ -1,13 +1,11 @@
-import mysql from 'mysql'
-var config =
-{
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'prueba',
-    port: 3306
+import mysql from "mysql";
+var config = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "intermedias",
+  port: 3306,
 };
 const conn = new mysql.createConnection(config);
-
 
 export default conn;
